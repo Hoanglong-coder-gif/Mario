0acbb7a4aa1a09d26fd613702e7c90e41540c6ae
DESKTOP-QOKVLGB\Hoang Long <hoanghaidangtc2003@gmail.com>
2025-02-26 11:24:55 +0700

    di chuyen and xep map

---
+++ Assets/Scripts/CharectorBase.cs
@@ -0,0 +1,77 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum ActionType
+{
+    Left,
+    Right,
+    Jump
+}
+
+public abstract class CharectorBase : MonoBehaviour
+{
+    public Animator animator;
+    public SpriteRenderer spriteRenderer;
+    public Rigidbody2D rigidbody2D;
+    public float jumpForce;
+    public float speed;
+    public bool groundCheck;
+    public abstract void Init();
+    public abstract void Hit();
+    //public virtual void Update()
+    //{
+
+    //}
+
+    private void Update()
+    {
+        if(Input.GetKey(KeyCode.LeftArrow))
+        {
+            Move(ActionType.Left);
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            Move(ActionType.Right);
+        }
+        if (Input.GetKey(KeyCode.Space))
+        {
+            Move(ActionType.Jump);
+        }
+        if(!Input.anyKey)
+        {
+            rigidbody2D.velocity = new Vector2(0, rigidbody2D.velocity.y);
+            animator.Play("Idle");
+        }
+    }
+
+    public virtual void Die()
+    {
+
+    }
+
+    public virtual void Move(ActionType actionTypeParam)
+    {
+        if(!groundCheck)
+        {
+            return;
+        }
+        switch(actionTypeParam)
+        {
+            case ActionType.Left:
+                rigidbody2D.velocity = new Vector2(-speed, rigidbody2D.velocity.y);
+                spriteRenderer.transform.localScale = new Vector3(-1, 1, 1);
+                animator.Play("Move");
+                break;
+            case ActionType.Right:
+                rigidbody2D.velocity = new Vector2(speed, rigidbody2D.velocity.y);
+                spriteRenderer.transform.localScale = new Vector3(1, 1, 1);
+                animator.Play("Move");
+                break;
+            case ActionType.Jump:                 
+                animator.Play("Jump");
+                rigidbody2D.AddForce(new Vector2(rigidbody2D.velocity.x, 2 * jumpForce));
+                break;
+        }
+    }
+}
---
+++ Assets/Scripts/SmallCharector.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SmallCharector : CharectorBase
+{
+    public override void Hit()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override void Init()
+    {
+        throw new System.NotImplementedException();
+    }
+
+}
